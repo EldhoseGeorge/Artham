12dcd95fad335f887462980ab22accc13d10440a
eldhose252 <eldhose252@gmail.com>
2025-07-14 22:35:04 +0530

    code change for init db in oninstall

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,15 @@
 import { Meaning, Word, Dictionary } from "./types";
 const db_name: string = "db_dictionary";
 const object_store_name: string = "engmal";
-
+const db_version: number = 1;
 function getDictionaryData(): Promise<Dictionary> {
   return fetch(chrome.runtime.getURL("/data/enml.json"))
     .then((response) => response.json())
     .then((data: Dictionary) => data);
 }
 
-async function backgroundWorkerDBinit(): Promise<any> {
-  const request: IDBOpenDBRequest = indexedDB.open(db_name, 1);
+async function DBinit(): Promise<any> {
+  const request: IDBOpenDBRequest = indexedDB.open(db_name, db_version);
   request.onerror = (event) => {
     console.error(
       "Database error:",
@@ -18,23 +18,22 @@ async function backgroundWorkerDBinit(): Promise<any> {
   };
   request.onupgradeneeded = (event) => {
     const db = (event.target as IDBOpenDBRequest).result;
-    if (!db.objectStoreNames.contains(object_store_name)) {
-      console.log("Creating object store:", object_store_name);
-      const objectStore: IDBObjectStore = db.createObjectStore(
-        object_store_name,
-        { keyPath: "source" }
-      );
-      objectStore.transaction.oncomplete = (event) => {
-        getDictionaryData().then((data: Dictionary) => {
-          const dataObjectStore: IDBObjectStore = db
-            .transaction(object_store_name, "readwrite")
-            .objectStore(object_store_name);
-          data?.data.forEach((item: Word) => {
-            dataObjectStore.add(item);
-          });
+
+    console.log("Creating object store:", object_store_name);
+    const objectStore: IDBObjectStore = db.createObjectStore(
+      object_store_name,
+      { keyPath: "source" }
+    );
+    objectStore.transaction.oncomplete = (event) => {
+      getDictionaryData().then((data: Dictionary) => {
+        const dataObjectStore: IDBObjectStore = db
+          .transaction(object_store_name, "readwrite")
+          .objectStore(object_store_name);
+        data?.data.forEach((item: Word) => {
+          dataObjectStore.add(item);
         });
-      };
-    }
+      });
+    };
   };
   request.onsuccess = (event) => {
     const db = (event.target as IDBOpenDBRequest).result;
@@ -42,13 +41,16 @@ async function backgroundWorkerDBinit(): Promise<any> {
   };
 }
 
-backgroundWorkerDBinit();
+chrome.runtime.onInstalled.addListener(() => {
+  DBinit();
+  return true;
+});
 
 chrome.runtime.onMessage.addListener(
   (request: { action: string; word: string }, sender, sendResponse) => {
     if (request.action === "getMeaning") {
       const word = request.word.toLowerCase().trim();
-      const dbRequest: IDBOpenDBRequest = indexedDB.open(db_name, 1);
+      const dbRequest: IDBOpenDBRequest = indexedDB.open(db_name, db_version);
       dbRequest.onsuccess = (event) => {
         const db: IDBDatabase = (event.target as IDBOpenDBRequest).result;
         const transaction: IDBTransaction = db.transaction(
