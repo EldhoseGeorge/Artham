089d1ce0a9df3155aa60b8c3a2443d92886d8dd1
eldhose252 <eldhose252@gmail.com>
2025-07-17 00:05:06 +0530

    added language fix ui changes

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,66 @@
-import { Meaning } from "./types";
+import { Meaning, type_map } from "./types";
 const tooltipID = "dictionary-tooltip";
+
+function createWorldBlock(type: string, meanings: string[]): HTMLElement {
+  const block = document.createElement("div");
+  block.style.marginBottom = "10px";
+  block.style.padding = "5px";
+  const typeLabel = document.createElement("strong");
+  typeLabel.textContent = type;
+  typeLabel.style.display = "block";
+  typeLabel.style.marginBottom = "5px";
+  block.appendChild(typeLabel);
+  const meaningBloock = document.createElement("div");
+  meaningBloock.style.marginLeft = "10px";
+  meaningBloock.style.display = "flex";
+  meaningBloock.style.flexDirection = "row";
+  meaningBloock.style.flexWrap = "wrap";
+
+  meanings.forEach((meaning) => {
+    const meaningText = document.createElement("span");
+    meaningText.textContent = meaning + ",";
+    meaningText.style.display = "block";
+    meaningText.style.padding = "2px 5px";
+    meaningText.style.marginBottom = "3px";
+    meaningBloock.appendChild(meaningText);
+  });
+  block.appendChild(meaningBloock);
+  return block;
+}
+
+function createTooltip(data: Meaning[], id: string): HTMLElement {
+  const tooltip = document.createElement("div");
+
+  tooltip.id = id;
+  tooltip.style.display = "flex";
+  tooltip.style.flexDirection = "column";
+  tooltip.style.position = "absolute";
+  tooltip.style.border = "1px solid #ccc";
+  tooltip.style.padding = "10px";
+  tooltip.style.zIndex = "1000";
+  tooltip.style.backgroundColor = "rgb(123, 211, 234)";
+  tooltip.style.color = "black";
+  tooltip.style.borderRadius = "5px";
+  tooltip.style.fontFamily =
+    "'Helvetica Neue', 'Segoe UI', Helvetica, sans-serif";
+  let sortedMeaning: { [key: string]: string[] } = {};
+
+  data.forEach((word) => {
+    let _type: string = type_map[word.type] || word.type;
+    if (!sortedMeaning[_type]) {
+      sortedMeaning[_type] = [];
+    }
+    sortedMeaning[_type].push(word.meaning);
+  });
+  for (const type in sortedMeaning) {
+    const meanings = sortedMeaning[type];
+    const block = createWorldBlock(type, meanings);
+    tooltip.appendChild(block);
+  }
+
+  return tooltip;
+}
+
 document.addEventListener("mouseup", (event) => {
   const existingtooltip = document.getElementById(tooltipID);
   if (existingtooltip) {
@@ -23,34 +84,19 @@ document.addEventListener("mouseup", (event) => {
       if (result.length > 0) {
         //console.log("Received meaning:", result);
 
-        const tooltip = document.createElement("div");
-        tooltip.id = tooltipID;
-        tooltip.style.position = "absolute";
-        tooltip.style.border = "1px solid #ccc";
-        tooltip.style.padding = "10px";
-        tooltip.style.zIndex = "1000";
-        tooltip.style.backgroundColor = "rgba(148, 145, 145, 0.91)";
-        tooltip.style.color = "black";
-        tooltip.style.borderRadius = "5px";
-        let sortedMeaning: { [key: string]: string[] } = {};
-        result.forEach((word) => {
-          if (!sortedMeaning[word.type]) {
-            sortedMeaning[word.type] = [];
-          }
-          sortedMeaning[word.type].push(word.meaning);
-        });
-        tooltip.innerHTML = Object.entries(sortedMeaning)
-          .map(([type, meanings]) => `<em>${type}:</em> ${meanings.join(", ")}`)
-          .join("<br>");
+        const tooltip = createTooltip(result, tooltipID);
+        document.documentElement.lang = "ml";
         document.body.appendChild(tooltip);
-
-        const rect = selection.getRangeAt(0).getBoundingClientRect();
-        tooltip.style.left = `${rect.left}px`;
-        tooltip.style.top = `${rect.bottom + window.scrollY}px`;
-        console.log(tooltip);
+        if (selection.rangeCount > 0) {
+          const rect = selection.getRangeAt(0).getBoundingClientRect();
+          tooltip.style.left = `${rect.left}px`;
+          tooltip.style.top = `${rect.bottom + window.scrollY}px`;
+          console.log(tooltip);
+        }
         setTimeout(() => {
           document.body.removeChild(tooltip);
-        }, 5000); // Remove after 5 seconds
+        }, 5000);
+        // Remove after 5 seconds
       }
     });
   }
--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,34 @@ export interface Word {
 export interface Dictionary {
   data: Word[];
 }
+
+export const type_map: Record<string, string> = {
+  "{n}": "noun",
+  "{v}": "verb",
+  "{adj}": "adjective",
+  "{adv}": "adverb",
+  "{pron}": "pronoun",
+  "{prep}": "preposition",
+  "{conj}": "conjunction",
+  "{interj}": "interjection",
+  "{det}": "determiner",
+  "{num}": "numeral",
+  "{excl}": "exclamation",
+  "{abbr}": "abbreviation",
+  "{slang}": "slang",
+  "{idiom}": "idiom",
+  "{phrase}": "phrase",
+  "{coll}": "colloquialism",
+  "{archaic}": "archaic",
+  "{formal}": "formal",
+  "{informal}": "informal",
+  "{technical}": "technical",
+  "{dialect}": "dialect",
+  "{poetic}": "poetic",
+  "{obsolete}": "obsolete",
+  "{regional}": "regional",
+  "{jargon}": "jargon",
+  "{colloquial}": "colloquial",
+  "{a}": "adjective",
+  "{-}": "-",
+};
